77b3ee55610dc62d9ec64e909cbcc12efde32bf6
Akshay Shinde <s.yahska@gmail.com>
2016-01-17 12:57:16 +0530

    js modified.

--- script/DropZone.js
+++ script/DropZone.js
@@ -1,5 +1,22 @@
 $(window).load(function(){
-	console.log("Script Loaded");
-	var fileReader = new Reader();
+  console.log("Script Loaded");
+  var fileReader = new FileReader();
+  function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var files = evt.dataTransfer.files; // FileList object.
+    
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  // Setup the dnd listeners.
+  var dropZone = document.getElementById('file-selector');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
 
 });
